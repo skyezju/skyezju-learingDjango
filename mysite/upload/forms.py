0a916b4a567f133302a9a0ecaad6f96ff3d92993
Ying, Yuqian <yying@microstrategy.com>
2015-03-05 18:50:12 +0800

    add call java code

--- mysite/upload/forms.py
+++ mysite/upload/forms.py
@@ -1,13 +1,29 @@
 __author__ = 'Skye'
 from django import forms
 import xlrd
+import os
+import time
+import jpype
+import os.path
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
     file.label = "Upload file"
     TQMSID = "default"
     def readexcelFile(self):
-        book = xlrd.open_workbook("myfile.xls")
+        book = xlrd.open_workbook("./upload/uploadedfile/InputFile.xls")
+        table = book.sheets()[0]
+        nrows = table.nrows
+        for i in range(1,nrows):
+            row_value = table.row_values(i)
+            info_dic = {'tqmsID': row_value[0],
+                'rallyProjectName': row_value[2],
+                'ownerName':row_value[1],
+                'releaseName':row_value[3]}
+            validateData(info_dic)
+        # rename the readed file with time name to backup it.
+        os.rename("./upload/uploadedfile/InputFile.xls",
+                  "./upload/uploadedfile/InputFile"+"_"+ time.strftime('%Y-%m-%d %X',time.localtime())+"_"+".xls")
 
 
 class IssueForm(forms.Form):
@@ -19,61 +35,79 @@ class IssueForm(forms.Form):
     ownerName.label = "Owner"
     releaseName = forms.CharField()
     releaseName.label = "Release"
-
-    def clean_message(self):
-        message = self.cleaned_data['message']
-        num_words = len(message.split())
-        if num_words < 4:
-            raise forms.ValidationError("Not enough words!")
-        return message
-
-    def validateData(self):
-        isDataValidated = 0;
-        if(self.validateOwner()):
-            print("The owner is valid")
-            isDataValidated = isDataValidated+1
-        else:
-            print("The owner is invalid")
-
-        if(self.validateProject()):
-            print("The project is valid")
-            isDataValidated = isDataValidated+1
-        else:
-            print("The project is invalid")
-
-        if(self.validateRelease()):
-            print("The Releaes is valid")
-            isDataValidated = isDataValidated+1
-        else:
-            print("The Release is invalid")
-
-        if(self.validateTQMSID()):
-            print("The TQMS ID is valid")
-            isDataValidated = isDataValidated+1
-        else:
-            print("The TQMS ID is invalid")
-
-        if(isDataValidated == 4):
+    def validateDataAll(self):
+        info_dic = {'tqmsID': self.cleaned_data['tqmsID'],
+                'rallyProjectName': self.cleaned_data['rallyProjectName'],
+                'ownerName':self.cleaned_data['ownerName'],
+                'releaseName':self.cleaned_data['releaseName']}
+        if validateData(info_dic):
             return True
         else:
             return False
 
-    def validateTQMSID(self):
-        #call com.microstrategy.rally.tools.rallyobjects.RallyDefect.validationDefectByTQMSID with id
-        print(self.cleaned_data['tqmsID'])
-        return True
 
-    def validateProject(self):
-        #call com.microstrategy.rally.tools.rallyobjects.RallyProject.queryProjectByName
-        print(self.cleaned_data['rallyProjectName'])
-        return True
+def validateData(info_dic):
+    isDataValidated = 0;
+    if(validateOwner(info_dic['ownerName'])):
+        print("The owner is valid")
+        isDataValidated = isDataValidated+1
+    else:
+        print("The owner is invalid")
 
-    def validateOwner(self):
-        print(self.cleaned_data['ownerName'])
-        return True
+    if(validateProject(info_dic['rallyProjectName'])):
+        print("The project is valid")
+        isDataValidated = isDataValidated+1
+    else:
+        print("The project is invalid")
 
-    def validateRelease(self):
-        print(self.cleaned_data['releaseName'])
-        return  True
+    if(validateRelease(info_dic['releaseName'])):
+        print("The Releaes is valid")
+        isDataValidated = isDataValidated+1
+    else:
+        print("The Release is invalid")
+
+    if(validateTQMSID(info_dic['tqmsID'])):
+        print("The TQMS ID is valid")
+        isDataValidated = isDataValidated+1
+    else:
+        print("The TQMS ID is invalid")
+
+    if(isDataValidated == 4):
+        return True
+    else:
+        return False
+
+def validateTQMSID(tqmsID):
+    #call com.microstrategy.rally.tools.rallyobjects.RallyDefect.validationDefectByTQMSID with id
+    #/Users/Skye/Documents/DevOps/djangoTry/mysite/upload
+    jvmPath = jpype.getDefaultJVMPath()
+
+    ext_classpath ="/Users/Skye/Documents/DevOps/djangoTry/mysite/upload/Rally.jar"
+    #ext_classpath ="/Users/Skye/Documents/DevOps/djangoTry/mysite/upload/Rally/com/microstrategy/rally/tools"
+    jvmArg = "-Djava.class.path=%s" % (ext_classpath)
+    if not jpype.isJVMStarted():
+        #jpype.startJVM(jvmPath, jvmArg)
+        jpype.startJVM(jvmPath, "-Xms32m", "-Xmx256m", "-mx256m",jvmArg)
+    JDClass =  jpype.JClass("com.microstrategy.rally.tools.UploadTQMS2RallyFromDB")
+    #JDClass =  jpype.JClass("UploadTQMS2RallyFromDB")
+    jd = JDClass()
+    jprint = jpype.java.lang.System.out.println
+    jprint(jd.validateTQMSID(tqmsID))
+    jpype.shutdownJVM()
+    print(tqmsID)
+    return True
+
+def validateProject(rallyProjectName):
+    #call com.microstrategy.rally.tools.rallyobjects.RallyProject.queryProjectByName
+    print(rallyProjectName)
+    return True
+
+def validateOwner(ownerName):
+    print(ownerName)
+    return True
+
+def validateRelease(releaseName):
+    print(releaseName)
+    return  True
 
 
--- mysite/upload/views.py
+++ mysite/upload/views.py
@@ -3,8 +3,8 @@ from upload.models import viewNavigation,uploadfromfile,uploadfromtext
 from django.shortcuts import *
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
-from upload.forms import IssueForm
-from .forms import UploadFileForm
+from upload.forms import IssueForm, UploadFileForm
+
 
 
 # Create your views here.
@@ -19,7 +19,7 @@ def upload(request):
     if request.method == 'POST':
         form = IssueForm(request.POST)
         if form.is_valid():
-            if form.validateData():
+            if form.validateDataAll():
                 #upload the data
 
                 #clean the form
@@ -32,7 +32,7 @@ def upload(request):
 
 
 def handle_uploaded_file(f):
-    f_path='./upload/uploadedfile/'+f.name
+    f_path='./upload/uploadedfile/InputFile.xls'
     with open(f_path, 'wb+') as info:
         print f.name
         for chunk in f.chunks():
@@ -44,6 +44,7 @@ def upload_file(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
+            form.readexcelFile()
             return render_to_response('upload/file.html', {'form': form},context_instance = RequestContext(request))
     else:
         form = UploadFileForm()
